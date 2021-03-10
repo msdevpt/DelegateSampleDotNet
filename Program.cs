c514574439efcea74760705648f717fae0f42b23
M.Silva <msilva@kiubo.pt>
2021-03-10 18:42:23 +0000

    1st init

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DelegateSample
+{
+    class Program
+    {
+        private delegate void GreetingDelegate(string msgString);
+
+        private static void GoodMoring(string yourName)
+        {
+            Console.WriteLine("Good Morning " + yourName + " !");
+        }
+        private static void GoodNight(string yourName)
+        {
+            Console.WriteLine("Good Night " + yourName + " !");
+        }
+    
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adding 'GoodMoring' Reference to a Delegate...");
+            GreetingDelegate myGreeting = GoodMoring;
+
+            // invoking the delegate
+            Console.WriteLine("Invokin Delegate...");
+            myGreeting.Invoke("MS");
+
+            // Assiging the addressof the another function to the same delegate
+            Console.WriteLine();
+            Console.WriteLine("Making Existing Delegate To Point To Another Fuction...");
+            Console.WriteLine("Replacing With 'GoodNight' Reference...");
+            myGreeting = new GreetingDelegate(GoodNight);
+
+            // Another way of invoking the delegate
+            Console.WriteLine("Invoking Delegate...");
+            myGreeting("SM");
+
+        }
+    }
+}
